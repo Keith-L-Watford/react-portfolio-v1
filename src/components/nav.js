c66d96e4d65539fa49b25fe21ff4f7aaffbcba9a
Keith Watford <keith.l.watford@gmail.com>
2021-08-03 21:07:19 -0400

    finished the file for now

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,15 @@
 import React from 'react'
+// import '../styles/nav.css'
+import '../styles/nav.css'
 
 export default function Nav() {
     return(
-        <div>
-        </div>
+        <ul>
+            <li>About Me</li>
+            <li>Projects</li>
+            {/* Maybe resume will have its own section, maybe in projects. idk */}
+            {/* <li>Resume</li> */}
+            <li>Contact Me</li>
+        </ul>
     );
 }
