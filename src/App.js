ba7a3a3ce73d435cfc077cbea862e98649ddab84
Keith Watford <keith.l.watford@gmail.com>
2021-08-04 21:32:40 -0400

    rerouted the files. added one for home but its off for now

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import './styles/app.css'
 import Header from './components/header.js'
 import Nav from './components/nav'
-import Projects from './components/projects'
-import Contact from './components/contact'
+import Projects from './components/pages/projects'
+import Contact from './components/pages/contact'
+// import Home from './components/pages/home'
 
 function App() {
   return <>
