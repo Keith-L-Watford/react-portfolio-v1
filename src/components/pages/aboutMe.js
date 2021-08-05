6c256e2ac79daea10c202a444bc69227887e1e82
Keith Watford <keith.l.watford@gmail.com>
2021-08-05 19:21:05 -0400

    finihed a draft of this page

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import '../../styles/pages.css';
 
 export default function AboutMe() {
-    return(
-        <div>
-            about me section
+  return (
+    <div >
+      <h4 className="content title" >Hello!</h4>
+
+      <br />
+      <div className="columns">
+        <div className="column" id="sentenceOne">
+          I am driven Theatre Artist / Preschool Teacher's Assistant turned
+          Full-Stack Web Developer.
         </div>
-    );
+        <br />
+        <div className="column" id="sentenceTwo">
+          I am well versed in team collaboration to formulate and conceptualize
+          a goal or product.
+        </div>
+        <br />
+        <div className="column" id="sentenceThree">
+          I am highly organized and focused on creating clean and logical code.
+        </div>
+        <br />
+        <div className="column" id="sentenceFour">I am a team-player experienced in HTML, CSS, and JavaScript.</div>
+      </div>
+    </div>
+  );
 }
