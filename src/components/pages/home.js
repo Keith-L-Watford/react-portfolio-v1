b8dae3432bbc4a2dd2ea4a696a750eea02b86d3f
Keith Watford <keith.l.watford@gmail.com>
2021-08-05 19:23:37 -0400

    changed to welcome, I would like to add text animations in the future

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../../styles/home.css"
 
-export default function AboutMe() {
+export default function Home() {
     return(
         <div>
-            <h1>Home</h1>
+            <h1 className="title char">Welcome!</h1>
         </div>
     );
 }
