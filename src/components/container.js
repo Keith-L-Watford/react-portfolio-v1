1776883095a652a77b3aeee3e9f58a852941881c
Keith Watford <keith.l.watford@gmail.com>
2021-08-05 00:04:57 -0400

    mostly got the container working. Just need to move the content over

--- src/components/container.js
+++ src/components/container.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Nav from './components/nav'
-import AboutMe from './components/pages/aboutMe'
-import Projects from './components/pages/projects'
-import Contact from './components/pages/contact'
-import Home from './components/pages/home'
-
+import Nav from "./nav";
+import AboutMe from "./pages/aboutMe";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+import Home from "./pages/home";
 
 export default function PortfolioContainer() {
-    const [currentPage, setCurrentPage] = useState('Home');
-  
-    // TODO: Add a comment describing the functionality of this method
-    const renderPage = () => {
-      if (currentPage === 'Home') {
-        return <Home />;
-      }
-      if (currentPage === 'AboutMe') {
-        return <AboutMe />;
-      }
-      if (currentPage === 'Projects') {
-        return <Projects />;
-      }
-      return <Contact />;
-    };
-  
-    const handlePageChange = (page) => setCurrentPage(page);
-  
-    return (
-      <div>
-        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-      </div>
-    );
-  }
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "AboutMe") {
+      return <AboutMe />;
+    }
+    if (currentPage === "Projects") {
+      return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
