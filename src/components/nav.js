a197b0a990ad2949d62e61bc774de501c4432134
Keith Watford <keith.l.watford@gmail.com>
2021-08-05 23:26:22 -0400

    got to an mvp nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,11 +7,9 @@ export default function Nav({ currentPage, handlePageChange }) {
 
 
 <nav className="navbar" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand">
-    <a className="navbar-item" href="https://bulma.io">
-      {/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" /> */}
-    </a>
-  </div>
+  {/* <div className="navbar-brand">
+    
+  </div> */}
 
   <div id="navbarBasicExample" className="navbar-menu center">
     <div className="navbar-start">
