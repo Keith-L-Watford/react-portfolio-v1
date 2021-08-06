5ff2e4f2c0b37277eb141660e9c9b73b592051be
Keith Watford <keith.l.watford@gmail.com>
2021-08-05 20:19:35 -0400

    removed an unused class

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,7 +4,7 @@ import "../../styles/home.css"
 export default function Home() {
     return(
         <div>
-            <h1 className="title char">Welcome!</h1>
+            <h1 className="title">Welcome!</h1>
         </div>
     );
 }
